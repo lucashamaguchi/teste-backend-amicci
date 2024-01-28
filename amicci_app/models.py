16d77aaa13a67f7db528d5f3c0711a246113e0c0
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-28 14:52:14 -0300

    add updated_at and created_at to category

--- amicci_app/models.py
+++ amicci_app/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
