c7e93c27ff9d921f2f51f07a9577b190add4b221
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-28 15:08:50 -0300

    setup models

--- amicci_app/models.py
+++ amicci_app/models.py
@@ -7,3 +7,27 @@ class Category(models.Model):
     description = models.CharField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Retailer(models.Model):
+    name = models.CharField(max_length=200)
+    vendors = models.ManyToManyField(Vendor, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Briefing(models.Model):
+    name = models.CharField()
+    retailer = models.OneToOneField(Retailer, models.DO_NOTHING, blank=True)
+    responsible = models.CharField()
+    category = models.CharField()
+    release_date = models.CharField()
+    available = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
