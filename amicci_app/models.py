69c6d2c86814256818473c87560ce1cb3bdc5b6c
lucashamaguchi <lucas@hamaguchi.com.br>
2024-01-28 15:44:07 -0300

    fix column type

--- amicci_app/models.py
+++ amicci_app/models.py
@@ -8,11 +8,17 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Vendor(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Retailer(models.Model):
@@ -21,13 +27,19 @@ class Retailer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Briefing(models.Model):
     name = models.CharField()
     retailer = models.OneToOneField(Retailer, models.DO_NOTHING, blank=True)
     responsible = models.CharField()
-    category = models.CharField()
+    category = models.OneToOneField(Category, models.DO_NOTHING, blank=True)
     release_date = models.CharField()
     available = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
